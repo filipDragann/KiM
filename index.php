9e180f4e0b75680bf7a9b02d826e3e258a615446
Filip <filipdragan2004@gmail.com>
2025-04-29 15:03:02 +0300

    testare

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titlu111</title>
-</head>
-<body>
+<?php
+    $name = "Filip Dragan";
+    $food = "pizza";
+    $email = "email@email.com";
+
+    $age = 21;
+    $users = 2;
     
-</body>
-</html>
+
+    
+?>
+
+
