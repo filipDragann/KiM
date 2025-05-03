1dfac864794c76014d9b96cfd403fe5627ad0a96
filip <filipdrgn@outlook.com>
2025-05-03 13:20:31 +0200

    Structurare Proiect

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titlu111</title>
+    <title>Titlu</title>
 </head>
 <body>
     
