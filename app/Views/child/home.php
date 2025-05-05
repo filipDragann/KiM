f3aab410f25f7f7ffd28df4c09316ddc3232ec7b
filipDragann <filipdragan2004@gmail.com>
2025-05-05 15:26:38 +0200

    Structurare Proiect

---
+++ app/Views/child/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Child Interface</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+// This is the child interface view file.
+// It is a simple HTML structure with a title.      
+
+?>
